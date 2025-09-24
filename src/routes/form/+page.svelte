101c83fb84eada1c21f33ebd39f45d482c9a85a3
Thomas Boland <thomas@thomasjonas.nl>
2025-09-24 12:20:08 +0200

    test form

---
+++ src/routes/form/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import MetaForm from '$lib/components/MetaForm.svelte';
+</script>
+
+<div class="p-5">
+	<MetaForm
+		video={{ id: 'asd', duration: 0, size: 0, description: '', filename: 'asd' }}
+		onError={() => {}}
+		restart={() => {}}
+	/>
+</div>
