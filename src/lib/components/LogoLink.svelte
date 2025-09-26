cc2a12fc5abd55b90f93a7cfe4b10719ddb43309
Thomas Boland <thomas@thomasjonas.nl>
2025-09-26 09:31:28 +0200

    Logo fix on Safari

--- src/lib/components/LogoLink.svelte
+++ src/lib/components/LogoLink.svelte
@@ -2,7 +2,7 @@
 	let { link, image, name } = $props();
 </script>
 
-<div class="w-1/3 px-4 pt-4 pb-2 lg:px-10 lg:pt-10 lg:pb-5">
+<div class="w-1/3 bg-green px-4 pt-4 pb-2 lg:px-10 lg:pt-10 lg:pb-5">
 	<a href={link} target="_blank">
 		<img src={image} alt="Logo for {name}" class="mix-blend-multiply" />
 	</a>
