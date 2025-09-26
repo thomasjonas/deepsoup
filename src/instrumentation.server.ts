b70b77d23686c55a2fcedc5b0fa792ee3d7ba922
Thomas Boland <thomas@thomasjonas.nl>
2025-09-26 13:29:18 +0200

    disable Sentry in dev

--- src/instrumentation.server.ts
+++ src/instrumentation.server.ts
@@ -1,11 +1,13 @@
+import { dev } from '$app/environment';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-  dsn: 'https://72bb061f1d75fad6034806c060b7246c@o4510079347064832.ingest.de.sentry.io/4510079348506704',
+	dsn: 'https://72bb061f1d75fad6034806c060b7246c@o4510079347064832.ingest.de.sentry.io/4510079348506704',
+	enabled: !dev,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+	// Enable logs to be sent to Sentry
+	enableLogs: true
 
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: import.meta.env.DEV,
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: import.meta.env.DEV,
 });
