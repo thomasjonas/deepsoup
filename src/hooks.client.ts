2639daac05b1bc29601cbe78ad837767c946de74
Thomas Boland <thomas@thomasjonas.nl>
2025-09-26 12:15:07 +0200

    disable Sentry in dev

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,22 +1,24 @@
-import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
+import { dev } from '$app/environment';
+import { handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
-  dsn: 'https://72bb061f1d75fad6034806c060b7246c@o4510079347064832.ingest.de.sentry.io/4510079348506704',
+	dsn: 'https://72bb061f1d75fad6034806c060b7246c@o4510079347064832.ingest.de.sentry.io/4510079348506704',
+	enabled: !dev,
 
-  // Enable logs to be sent to Sentry
-  enableLogs: true,
+	// Enable logs to be sent to Sentry
+	enableLogs: true,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+	// This sets the sample rate to be 10%. You may want this to be 100% while
+	// in development and sample at a lower rate in production
+	replaysSessionSampleRate: 0.1,
 
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+	// If the entire session is not sampled, use the below sample rate to sample
+	// sessions when an error occurs.
+	replaysOnErrorSampleRate: 1.0,
 
-  // If you don't want to use Session Replay, just remove the line below:
-  integrations: [replayIntegration()],
+	// If you don't want to use Session Replay, just remove the line below:
+	integrations: [replayIntegration()]
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
